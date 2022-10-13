adc8d1ec982c965f41054257b996e19f97b1d8f1
CryingCoder <112829574+CryingCoder@users.noreply.github.com>
2022-10-13 13:23:48 -0400

    GitIgnore Example

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GitIgnore_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is an example");
+        }
+    }
+}
